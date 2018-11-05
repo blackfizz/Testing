ec169abc6fb21f2377fda5f12ebd56ef669eb865
Paul Cech <pc@plazz.ag>
2018-11-05 17:04:16 +0100

    -

--- scripts/build.sh
+++ scripts/build.sh
@@ -120,16 +120,3 @@ cat << "EOF"
                     ''''   (//|/\      ,';':,-'         `-.__  `'--..__
                                                              `''---::::'
 EOF
-© 2018 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-Press h to open a hovercard with more details.

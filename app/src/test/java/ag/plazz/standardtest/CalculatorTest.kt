6cf10ffcc42af7a7ac732f535f4de18fbb39dcda
Paul Cech <pc@plazz.ag>
2018-11-05 17:19:11 +0100

    added dummy test

---
+++ app/src/test/java/ag/plazz/standardtest/CalculatorTest.kt
@@ -0,0 +1,27 @@
+package ag.plazz.standardtest
+
+import org.junit.Assert
+import org.junit.Test
+
+/**
+ *   @Author Paul Cech
+ *   Created on 05.11.18
+ */
+class CalculatorTest {
+
+    @Test
+    fun multiply_check1() {
+        val result = Calculator.multiplyCorrect(3, 4)
+        val expectedResult = 12
+
+        Assert.assertEquals(expectedResult, result)
+    }
+
+    @Test
+    fun multiply_check2() {
+        val result = Calculator.multiplyWrong(3, 4)
+        val expectedResult = 12
+
+        Assert.assertEquals(expectedResult, result)
+    }
+}
